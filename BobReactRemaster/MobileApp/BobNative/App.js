2c72b9537f8238c17cdaeaddcfc36a3f95ec93c2
Benjamin Kreft <benjamin@kreft.com>
2022-05-07 19:35:29 +0200

    Mobile app Init

---
+++ BobReactRemaster/MobileApp/BobNative/App.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import type {Node} from 'react';
+import Login from './Screens/Login'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="login" component={Login} options={{title: "BobNative"}} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+/*
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+*/
+
+export default App;
---
+++ BobReactRemaster/MobileApp/BobNative/Screens/Login.js
@@ -0,0 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+//import 'react-native-gesture-handler';
+import {Text, View, TextInput, Image, TouchableOpacity} from 'react-native';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+      return (<View>
+          <Text>Test</Text>
+      </View>)
+  }
+}
